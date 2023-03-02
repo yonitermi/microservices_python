79077677f3026359f387f0e44da89bff6d5a37f7
Yoni <yonitermi@gmail.com>
2023-03-02 15:35:08 +0200

    Added requirments/dockerfiles

--- src/app.py
+++ src/app.py
@@ -1,7 +1,29 @@
-from flask import Flask
+from flask import Flask , jsonify, render_template
+import socket
 
 app = Flask(__name__)
+
+def fetchdetails():
+    hostname = socket.gethostname()
+    host_ip = socket.gethostbyname(hostname)
+    return str(hostname), str(host_ip)
+
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("/health")
+def health():
+    return jsonify(
+        status = "UP"
+    )
+
+@app.route("/details")
+def details():
+    hostname, ip = fetchdetails()
+    return render_template('index.html' , HOSTNAME = hostname , IP = ip)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
